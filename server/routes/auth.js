f3360fd52ccc8cf526f43a37a19d81f7bbdd5e73
shiradiamant1-oss <shiradiamant1@gmail.com>
2025-11-12 13:35:34 +0200

    Fix import case for User model in auth.js

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const nodemailer = require('nodemailer');
-const User = require('../models/User');
+const User = require('../models/user');
 const crypto = require('crypto');
 const rateLimit = require('express-rate-limit');
 
